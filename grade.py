10d1f85df53c9f6371e157d013b18f24cb46d91c
SejalPatil07 <patil.sejal0706@gmail.com>
2026-09-19 12:25:38 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,13 @@
+def calculate_grade(percentage: int):
+    if percentage >= 90:
+        return "A"
+    elif percentage >= 80:
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    elif percentage >= 50:
+        return "E"
+    else:
+        return "F"
